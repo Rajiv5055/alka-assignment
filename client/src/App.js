e885f846b45b4481a22b0f1bf6f7c24b46611953
Rajiv5055 <69000253+Rajiv5055@users.noreply.github.com>
2022-06-07 20:14:44 +0530

    plaid functionality is added
    plaid link open functionality and after connecting account. transactions is shown here.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,54 @@
 import React from 'react';
-import './App.css';
-import LinkButton from "./components/LinkButton";
-import { Route, withRouter, Switch } from 'react-router-dom';
+import "./App.css";
+import Link from "./components/Link";
 import axios from "axios";
- 
+import TransactionsContainer from './components/TransactionsContainer'
+import { Route, withRouter, Switch } from 'react-router-dom';
+
 class App extends React.Component {
+  state = {
+            token: null, 
+            access_token: null
+          }
+
+  //connects to plaid to create temporary link token
+  createLinkToken = async () => {
+    const res = await axios.post('http://localhost:5000/create_link_token');
+    const data = res.data.link_token
+    this.setState({ token: data })
+  }
+
+  //creates user link token upon page load
+  componentDidMount(){
+    this.createLinkToken()
+  }  
+
+ //ADD CODE BELOW
+ //if link token is successfully created, user can click on button to exchange public token for an access token
+  getAccessToken = async (publicToken) => {
+    console.log("client side public token", publicToken)
+    //sends the public token to the app server
+    const res = await axios.post('http://localhost:5000/get_access_token', {publicToken: publicToken})
+    const data = res.data.access_token
+    //updates state with permanent access token
+    this.setState({ access_token: data})
+     this.props.history.push("/home")
+  }
+
 
   render(){
     return (
       <>
       <div className="App">
-        <LinkButton/>
+         {this.state.access_token === null ? <Link token={this.state.token} accessToken={this.state.access_token} getAccessToken={this.getAccessToken} /> : 
+        <Switch>
+          <Route path="/home" render={(routerprops) =><TransactionsContainer accessToken={this.state.access_token} />} />
+        </Switch>
+      } 
       </div>
       </>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
