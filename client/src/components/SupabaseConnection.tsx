abcc9ad30f14b4bf51bd868957f86ce84bfb42f0
Rajiv5055 <69000253+Rajiv5055@users.noreply.github.com>
2022-06-16 21:47:56 +0530

    writing client in typescript

---
+++ client/src/components/SupabaseConnection.tsx
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient('https://ziqckeqitdhiowfafxlc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppcWNrZXFpdGRoaW93ZmFmeGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ2NjY2MzcsImV4cCI6MTk3MDI0MjYzN30.Ba7p22i7WHDjpqf-0JNAVy0Yk2eysarVNHspIl70mes')
+
+
+
