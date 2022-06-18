5773b6ab3485a760b6a8b3bbab5cf7f2ae975fbf
Rajiv5055 <69000253+Rajiv5055@users.noreply.github.com>
2022-06-18 18:11:42 +0530

    deleting console.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ class App extends React.Component<RouteComponentProps> {
 
  //if link token is successfully created, user can click on button to exchange public token for an access token
   getAccessToken = async (publicToken:any) => {
-    console.log(publicToken)
+    
     const res = await axios.post('http://localhost:5000/get_access_token', {publicToken: publicToken})
     const data = res.data.access_token
     
